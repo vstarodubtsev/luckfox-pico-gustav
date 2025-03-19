e54cbd14e652fa6b4c3ac66f03c9f8947ac82047
Vlad Starodubtsev <vlad.starodubtsev@keenetic.com>
2025-03-19 15:37:53 +0300

    gustavd 1.3.2
     * add SMS send

--- sysdrv/tools/board/buildroot/gustavd/gustavd.mk
+++ sysdrv/tools/board/buildroot/gustavd/gustavd.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GUSTAVD_VERSION = 1.3.1
+GUSTAVD_VERSION = 1.3.2
 GUSTAVD_SITE = https://github.com/vstarodubtsev/gustavd/archive/refs/tags/$(GUSTAVD_VERSION)
 GUSTAVD_LICENSE = GPL-2.0
 GUSTAVD_LICENSE_FILES = COPYING
