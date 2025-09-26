3148b1571752fea680ecbb8e1a4d9bf1ed4a49d5
Vlad Starodubtsev <vlad.starodubtsev@keenetic.com>
2025-09-26 14:25:07 +0300

    gustavd 1.5.1
     * introduce Gustav 5G

--- sysdrv/tools/board/buildroot/gustavd/gustavd.mk
+++ sysdrv/tools/board/buildroot/gustavd/gustavd.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-GUSTAVD_VERSION = 1.3.2
-GUSTAVD_SITE = https://github.com/vstarodubtsev/gustavd/archive/refs/tags/$(GUSTAVD_VERSION)
+GUSTAVD_VERSION = 1.5.1
+GUSTAVD_SITE = https://github.com/vstarodubtsev/gustavd/archive/$(GUSTAVD_VERSION)
 GUSTAVD_LICENSE = GPL-2.0
 GUSTAVD_LICENSE_FILES = COPYING
 
